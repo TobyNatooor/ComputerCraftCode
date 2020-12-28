1e749a7e23f515401d3c81ac3ac4f35f7d552775
tobiasnf <tobiasnf@gmail.com>
2020-12-28 17:02:54 +0100

    .

--- WSControlled.lua
+++ WSControlled.lua
@@ -1,7 +1,7 @@
 
 ws = http.websocket("ws://localhost:8080/")
 isBlock, blockData = turtle.inspectDown()
-ws.send(blockData.name .. ' ' .. 0 ..' ' .. -1 ..' ' .. 0)
+ws.send("From turtle: " .. blockData.name .. ' ' .. 0 ..' ' .. -1 ..' ' .. 0)
 
 while true do
     movement = ws.receive()
@@ -10,28 +10,22 @@ while true do
     
     if movement == "Up" then
         turtle.up()
-    end
-    if movement == "Forward" then
+    elseif movement == "Forward" then
         turtle.forward()
-    end
-    if movement == "Left" then
+    elseif movement == "Left" then
         turtle.turnLeft()
-    end
-    if movement == "Right" then
+    elseif movement == "Right" then
         turtle.turnRight()
-    end
-    if movement == "Back" then
+    elseif movement == "Back" then
         turtle.back()
-    end
-    if movement == "Down" then
+    elseif movement == "Down" then
         turtle.down()
-    end
-
-    if movement == "Refuel" then
+    elseif movement == "Refuel" then
         turtle.refuel()
-    end
-    if movement == "Dig" then
+    elseif movement == "Dig" then
         turtle.dig()
+    else
+        print("I don't don't recognise that command") 
     end
 end
 
